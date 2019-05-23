c6b2317833994ac879200705aac06fa63bcac78a
Michel Bechtold <michel.bechtold@kuleuven.be>
2019-05-23 14:19:49 +0200

    mkdir figpath

--- batchscripts/generate_standard_plots.py
+++ batchscripts/generate_standard_plots.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
+import os
 import platform
 if platform.system() == 'Linux':
     import matplotlib
     matplotlib.use('TkAgg')
 
-outpath = '/staging/leuven/stg_00024/OUTPUT/michelb/FIG_tmp'
 
 root='/scratch/leuven/317/vsc31786/output'
 exp = 'SMAP_EASEv2_M09_SI_CLSM_SMOSfw_DA'
 domain = 'SMAP_EASEv2_M09'
+
+
+outpath = '/staging/leuven/stg_00024/OUTPUT/michelb/FIG_tmp'
+os.makedirs(outpath,exist_ok=True)
 
 from bat_pyldas.plotting import plot_catparams
 plot_catparams(exp, domain, root, outpath)
