07f1af14f7dc564ddeddbc55aa0e5c1080bf78d6
Michel Bechtold <michel.bechtold@kuleuven.be>
2019-06-11 13:38:51 +0200

    DA diagnostics plots

--- functions.py
+++ functions.py
@@ -14,6 +14,290 @@ import logging
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 from collections import OrderedDict
 
+from pyldas.interface import LDAS_io
+
+from netCDF4 import Dataset
+
+def ncfile_init(fname, lats, lons, species, tags):
+
+    ds = Dataset(fname, 'w', 'NETCDF4')
+
+    dims = ['lat','lon','species']
+    dimvals = [lats, lons, species]
+    chunksizes = [len(lats), len(lons), 1]
+    dtypes = ['float32','float32','uint8']
+
+    for dim, dimval, chunksize, dtype in zip(dims, dimvals, chunksizes, dtypes):
+        ds.createDimension(dim, len(dimval))
+        ds.createVariable(dim, dtype, dimensions=[dim], chunksizes=[chunksize], zlib=True)
+        ds.variables[dim] = dimval
+
+    for tag in tags:
+        if tag.find('innov') != -1:
+            ds.createVariable(tag, 'float32', dimensions=dims, chunksizes=chunksizes, fill_value=-9999., zlib=True)
+        else:
+            ds.createVariable(tag, 'float32', dimensions=dims[0:-1], chunksizes=chunksizes[0:-1], fill_value=-9999., zlib=True)
+
+    return ds
+
+def ncfile_init_scaling(fname, lats, lons, pentad, AD, tags):
+
+    ds = Dataset(fname, 'w', 'NETCDF4')
+
+    dims = ['lat','lon','pentad','AD']
+    dimvals = [lats, lons, pentad, AD]
+    chunksizes = [len(lats), len(lons),1 ,1]
+    dtypes = ['float32','float32','uint8','uint8']
+
+    for dim, dimval, chunksize, dtype in zip(dims, dimvals, chunksizes, dtypes):
+        ds.createDimension(dim, len(dimval))
+        ds.createVariable(dim, dtype, dimensions=[dim], chunksizes=[chunksize], zlib=True)
+        ds.variables[dim] = dimval
+
+    for tag in tags:
+        ds.createVariable(tag, 'float32', dimensions=dims, chunksizes=chunksizes, fill_value=-9999., zlib=True)
+
+    return ds
+
+def ncfile_init_multiruns(fname, lats, lons, runs, species, tags):
+
+    ds = Dataset(fname, 'w', 'NETCDF4')
+
+    dims = ['lat','lon','run','species']
+    dimvals = [lats, lons, runs, species]
+    chunksizes = [len(lats), len(lons), 1, 1]
+    dtypes = ['float32','float32','uint8', 'uint8']
+
+    for dim, dimval, chunksize, dtype in zip(dims, dimvals, chunksizes, dtypes):
+        ds.createDimension(dim, len(dimval))
+        ds.createVariable(dim, dtype, dimensions=[dim], chunksizes=[chunksize], zlib=True)
+        ds.variables[dim] = dimval
+
+    for tag in tags:
+        if tag.find('innov') != -1:
+            ds.createVariable(tag, 'float32', dimensions=dims, chunksizes=chunksizes, fill_value=-9999., zlib=True)
+        else:
+            ds.createVariable(tag, 'float32', dimensions=dims[0:-1], chunksizes=chunksizes[0:-1], fill_value=-9999., zlib=True)
+
+    return ds
+
+def bin2nc_scaling(exp, domain, root, outputpath):
+
+    angle = 40
+    if not os.path.exists(outputpath):
+        os.makedirs(outputpath,exist_ok=True)
+    result_file = outputpath + 'scaling.nc'
+    os.remove(result_file) if os.path.exists(result_file) else None
+
+    io = LDAS_io('ObsFcstAna',exp,domain,root)
+    [lons,lats,llcrnrlat,urcrnrlat,llcrnrlon,urcrnrlon] = setup_grid_grid_for_plot(io)
+    tc = io.grid.tilecoord
+    tg = io.grid.tilegrids
+    lons_1D = io.grid.ease_lons[tc['i_indg'].min():(tc['i_indg'].max()+1)]
+    lats_1D = io.grid.ease_lats[tc['j_indg'].min():(tc['j_indg'].max()+1)]
+    pentad = np.arange(1,74)
+    AD = np.arange(0,2)
+    scalepath = io.read_nml('ensupd')['ens_upd_inputs']['obs_param_nml'][20]['scalepath'].split()[0]
+    scalename = io.read_nml('ensupd')['ens_upd_inputs']['obs_param_nml'][20]['scalename'][5:].split()[0]
+
+    tags = ['m_mod_H_%2i'%angle,'m_mod_V_%2i'%angle,'m_obs_H_%2i'%angle,'m_obs_V_%2i'%angle]
+
+    ds = ncfile_init_scaling(result_file, lats_1D, lons_1D, pentad, AD, tags)
+
+    for i_AscDes,AscDes in enumerate(list(["A","D"])):
+        for i_pentad in np.arange(1,74):
+
+            logging.info('pentad %i' % (i_pentad))
+            fname = scalepath+scalename+'_'+AscDes+'_p'+"%02d" % (i_pentad,)+'.bin'
+            res = io.read_scaling_parameters(fname=fname)
+
+            res = res[['lon','lat','m_mod_H_%2i'%angle,'m_mod_V_%2i'%angle,'m_obs_H_%2i'%angle,'m_obs_V_%2i'%angle]]
+            res.replace(-9999.,np.nan,inplace=True)
+
+            img = np.full(lons.shape, np.nan)
+            img[tc.j_indg.values, tc.i_indg.values] = res['m_mod_H_%2i'%angle].values
+            img = obs_M09_to_M36(img)
+            data = np.ma.masked_invalid(img)
+            ds['m_mod_H_%2i'%angle][:,:,i_pentad-1,i_AscDes] = data
+
+            img = np.full(lons.shape, np.nan)
+            img[tc.j_indg.values, tc.i_indg.values] = res['m_mod_V_%2i'%angle].values
+            img = obs_M09_to_M36(img)
+            data = np.ma.masked_invalid(img)
+            ds['m_mod_V_%2i'%angle][:,:,i_pentad-1,i_AscDes] = data
+
+            img = np.full(lons.shape, np.nan)
+            img[tc.j_indg.values, tc.i_indg.values] = res['m_obs_H_%2i'%angle].values
+            img = obs_M09_to_M36(img)
+            data = np.ma.masked_invalid(img)
+            ds['m_obs_H_%2i'%angle][:,:,i_pentad-1,i_AscDes] = data
+
+            img = np.full(lons.shape, np.nan)
+            img[tc.j_indg.values, tc.i_indg.values] = res['m_obs_V_%2i'%angle].values
+            img = obs_M09_to_M36(img)
+            data = np.ma.masked_invalid(img)
+            ds['m_obs_V_%2i'%angle][:,:,i_pentad-1,i_AscDes] = data
+
+    ds.close()
+
+def filter_diagnostics_evaluation(exp, domain, root, outputpath):
+
+    if not os.path.exists(outputpath):
+        os.makedirs(outputpath,exist_ok=True)
+    result_file = outputpath + 'filter_diagnostics.nc'
+    os.remove(result_file) if os.path.exists(result_file) else None
+
+    DA_innov = LDAS_io('ObsFcstAna',exp, domain, root)
+
+    DA_incr = LDAS_io('incr',exp, domain, root)
+
+    tags = ['innov_mean','innov_var',
+            'norm_innov_mean','norm_innov_var',
+            'n_valid_innov',
+            'incr_catdef_mean','incr_catdef_var',
+            'incr_rzexc_mean','incr_rzexc_var',
+            'incr_srfexc_mean','incr_srfexc_var',
+            'n_valid_incr']
+
+    tc = DA_innov.grid.tilecoord
+    tg = DA_innov.grid.tilegrids
+    lons = DA_innov.grid.ease_lons[tc['i_indg'].min():(tc['i_indg'].max()+1)]
+    lats = DA_innov.grid.ease_lats[tc['j_indg'].min():(tc['j_indg'].max()+1)]
+    species = DA_innov.timeseries['species'].values
+
+    ds = ncfile_init(result_file, lats, lons, species, tags)
+
+    tmp = DA_incr.timeseries['srfexc'][:,:,:].values
+    np.place(tmp, tmp==0., np.nan)
+    np.place(tmp, ~np.isnan(tmp), 1.)
+    np.place(tmp, np.isnan(tmp), 0.)
+    ds['n_valid_incr'][:, :] = tmp.sum(axis=0)
+
+    for i_spc,spc in enumerate(species):
+
+        logging.info('species %i' % (i_spc))
+
+        ds['innov_mean'][:,:,i_spc] = (DA_innov.timeseries['obs_obs'][:,i_spc,:,:] - DA_innov.timeseries['obs_fcst'][:,i_spc,:,:]).mean(dim='time',skipna=True).values
+        ds['innov_var'][:,:,i_spc] = (DA_innov.timeseries['obs_obs'][:,i_spc,:,:] - DA_innov.timeseries['obs_fcst'][:,i_spc,:,:]).var(dim='time',skipna=True).values
+        ds['norm_innov_mean'][:,:,i_spc] = ((DA_innov.timeseries['obs_obs'][:,i_spc,:,:] - DA_innov.timeseries['obs_fcst'][:,i_spc,:,:]) /
+                                                  np.sqrt(DA_innov.timeseries['obs_obsvar'][:,i_spc,:,:] + DA_innov.timeseries['obs_fcstvar'][:,i_spc,:,:])).mean(dim='time',skipna=True).values
+        ds['norm_innov_var'][:,:,i_spc] = ((DA_innov.timeseries['obs_obs'][:,i_spc,:,:] - DA_innov.timeseries['obs_fcst'][:,i_spc,:,:]) /
+                                                 np.sqrt(DA_innov.timeseries['obs_obsvar'][:,i_spc,:,:] + DA_innov.timeseries['obs_fcstvar'][:,i_spc,:,:])).var(dim='time',skipna=True).values
+
+        #tmp = DA_innov.timeseries['obs_obs'][:,i_spc,:,:].values - DA_innov.timeseries['obs_fcst'][:,i_spc,:,:].values
+        tmp = DA_innov.timeseries['obs_ana'][:,i_spc,:,:].values
+        np.place(tmp, tmp==0., np.nan)
+        np.place(tmp, tmp==-9999., np.nan)
+        np.place(tmp, ~np.isnan(tmp), 1.)
+        np.place(tmp, np.isnan(tmp), 0.)
+        ds['n_valid_innov'][:, :, i_spc] = tmp.sum(axis=0)
+
+    np.place(DA_incr.timeseries['catdef'].values, DA_incr.timeseries['catdef'].values == 0, np.nan)
+    np.place(DA_incr.timeseries['rzexc'].values, DA_incr.timeseries['rzexc'].values == 0, np.nan)
+    np.place(DA_incr.timeseries['srfexc'].values, DA_incr.timeseries['srfexc'].values == 0, np.nan)
+    ds['incr_catdef_mean'][:, :] = DA_incr.timeseries['catdef'].mean(dim='time',skipna=True).values
+    ds['incr_catdef_var'][:, :] = DA_incr.timeseries['catdef'].var(dim='time',skipna=True).values
+    ds['incr_rzexc_mean'][:, :] = DA_incr.timeseries['rzexc'].mean(dim='time',skipna=True).values
+    ds['incr_rzexc_var'][:, :] = DA_incr.timeseries['rzexc'].var(dim='time',skipna=True).values
+    ds['incr_srfexc_mean'][:, :] = DA_incr.timeseries['srfexc'].mean(dim='time',skipna=True).values
+    ds['incr_srfexc_var'][:, :] = DA_incr.timeseries['srfexc'].var(dim='time',skipna=True).values
+
+    ds.close()
+
+def filter_diagnostics_evaluation_short(exp, domain, root, outputpath):
+
+    if not os.path.exists(outputpath):
+        os.makedirs(outputpath,exist_ok=True)
+    result_file = outputpath + 'filter_diagnostics_short.nc'
+    os.remove(result_file) if os.path.exists(result_file) else None
+
+    DA_innov = LDAS_io('ObsFcstAna',exp, domain, root)
+
+    DA_incr = LDAS_io('incr',exp, domain, root)
+
+    tags = ['n_valid_innov']
+
+    tc = DA_innov.grid.tilecoord
+    tg = DA_innov.grid.tilegrids
+    lons = DA_innov.grid.ease_lons[tc['i_indg'].min():(tc['i_indg'].max()+1)]
+    lats = DA_innov.grid.ease_lats[tc['j_indg'].min():(tc['j_indg'].max()+1)]
+    species = DA_innov.timeseries['species'].values
+
+    ds = ncfile_init(result_file, lats, lons, species, tags)
+    #row = 114
+    #col = 2342
+    for i_spc,spc in enumerate(species):
+
+        logging.info('species %i' % (i_spc))
+
+        tmp = DA_innov.timeseries['obs_ana'][:,i_spc,:,:].values
+        np.place(tmp, tmp==0., np.nan)
+        np.place(tmp, tmp==-9999., np.nan)
+        np.place(tmp, ~np.isnan(tmp), 1.)
+        np.place(tmp, np.isnan(tmp), 0.)
+        ds['n_valid_innov'][:, :, i_spc] = tmp.sum(axis=0)
+
+    ds.close()
+
+def filter_diagnostics_evaluation_compare(exp1, exp2, domain, root, outputpath):
+
+    result_file = outputpath + 'filter_diagnostics.nc'
+
+    DA_CLSM_innov = LDAS_io('ObsFcstAna',exp1, domain, root)
+    DA_PEAT_innov = LDAS_io('ObsFcstAna',exp2, domain, root)
+
+    DA_CLSM_incr = LDAS_io('incr',exp1, domain, root)
+    DA_PEAT_incr = LDAS_io('incr',exp2, domain, root)
+
+    runs = OrderedDict([(1,[DA_CLSM_innov.timeseries, DA_CLSM_incr.timeseries]),
+                        (2,[DA_PEAT_innov.timeseries, DA_PEAT_incr.timeseries])])
+
+    tags = ['innov_mean','innov_var',
+            'norm_innov_mean','norm_innov_var',
+            'n_valid_innov',
+            'incr_catdef_mean','incr_catdef_var',
+            'incr_rzexc_mean','incr_rzexc_var',
+            'incr_srfexc_mean','incr_srfexc_var']
+
+    tc = DA_CLSM_innov.grid.tilecoord
+    tg = DA_CLSM_innov.grid.tilegrids
+    lons = DA_CLSM_innov.grid.ease_lons[tc['i_indg'].min():(tc['i_indg'].max()+1)]
+    lats = DA_CLSM_innov.grid.ease_lats[tc['j_indg'].min():(tc['j_indg'].max()+1)]
+
+    species = DA_CLSM_innov.timeseries['species'].values
+
+    ds = ncfile_init(result_file, lats, lons, runs.keys(), species, tags)
+
+    for i_run,run in enumerate(runs):
+        for i_spc,spc in enumerate(species):
+
+            logging.info('run %i, species %i' % (i_run,i_spc))
+
+            ds['innov_mean'][:,:,i_run,i_spc] = (runs[run][0]['obs_obs'][:,i_spc,:,:] - runs[run][0]['obs_fcst'][:,i_spc,:,:]).mean(dim='time').values
+            ds['innov_var'][:,:,i_run,i_spc] = (runs[run][0]['obs_obs'][:,i_spc,:,:] - runs[run][0]['obs_fcst'][:,i_spc,:,:]).var(dim='time').values
+            ds['norm_innov_mean'][:,:,i_run,i_spc] = ((runs[run][0]['obs_obs'][:,i_spc,:,:] - runs[run][0]['obs_fcst'][:,i_spc,:,:]) /
+                                                      np.sqrt(runs[run][0]['obs_obsvar'][:,i_spc,:,:] + runs[run][0]['obs_fcstvar'][:,i_spc,:,:])).mean(dim='time').values
+            ds['norm_innov_var'][:,:,i_run,i_spc] = ((runs[run][0]['obs_obs'][:,i_spc,:,:] - runs[run][0]['obs_fcst'][:,i_spc,:,:]) /
+                                                     np.sqrt(runs[run][0]['obs_obsvar'][:,i_spc,:,:] + runs[run][0]['obs_fcstvar'][:,i_spc,:,:])).var(dim='time').values
+
+            tmp = runs[run][0]['obs_obs'][:,i_spc,:,:].values
+            np.place(tmp, ~np.isnan(tmp), 1.)
+            np.place(tmp, np.isnan(tmp), 0.)
+            ds['n_valid_innov'][:, :, i_run, i_spc] = tmp.sum(axis=0)
+
+        if len(runs[run]) == 2:
+            np.place(runs[run][1]['catdef'].values, runs[run][1]['catdef'].values == 0, np.nan)
+            np.place(runs[run][1]['rzexc'].values, runs[run][1]['rzexc'].values == 0, np.nan)
+            np.place(runs[run][1]['srfexc'].values, runs[run][1]['srfexc'].values == 0, np.nan)
+            ds['incr_catdef_mean'][:, :, i_run] = runs[run][1]['catdef'].mean(dim='time').values
+            ds['incr_catdef_var'][:, :, i_run] = runs[run][1]['catdef'].var(dim='time').values
+            ds['incr_rzexc_mean'][:, :, i_run] = runs[run][1]['rzexc'].mean(dim='time').values
+            ds['incr_rzexc_var'][:, :, i_run] = runs[run][1]['rzexc'].var(dim='time').values
+            ds['incr_srfexc_mean'][:, :, i_run] = runs[run][1]['srfexc'].mean(dim='time').values
+            ds['incr_srfexc_var'][:, :, i_run] = runs[run][1]['srfexc'].var(dim='time').values
+
+    ds.close()
+
 def estimate_tau(df, n_lags=180):
     """ Estimate characteristic time lengths for pd.DataFrame columns """
 
@@ -111,27 +395,43 @@ def obs_M09_to_M36(data):
     ydim_ind = np.where(np.nanmean(data,0)>-9e30)
     indmin=0
     indmax=np.size(ydim_ind)
-    if ydim_ind[0][0]<3:
-        indmin=1
-    if ydim_ind[0][-1]>(data.shape[1]-3):
-        indmax= np.size(ydim_ind)-1
-    data[:,ydim_ind[0][indmin:indmax]-1] = data[:,ydim_ind[0][indmin:indmax]]
-    data[:,ydim_ind[0][indmin:indmax]+1] = data[:,ydim_ind[0][indmin:indmax]]
-    data[:,ydim_ind[0][indmin:indmax]-2] = data[:,ydim_ind[0][indmin:indmax]]
-    data[:,ydim_ind[0][indmin:indmax]+2] = data[:,ydim_ind[0][indmin:indmax]]
+    try:
+        if np.size(ydim_ind[0])>0:
+            if ydim_ind[0][0]<3:
+                indmin=1
+            if ydim_ind[0][-1]>(data.shape[1]-3):
+                indmax= np.size(ydim_ind)-1
+            try:
+                data[:,ydim_ind[0][indmin:indmax]-1] = data[:,ydim_ind[0][indmin:indmax]].values
+                data[:,ydim_ind[0][indmin:indmax]+1] = data[:,ydim_ind[0][indmin:indmax]].values
+                data[:,ydim_ind[0][indmin:indmax]-2] = data[:,ydim_ind[0][indmin:indmax]].values
+                data[:,ydim_ind[0][indmin:indmax]+2] = data[:,ydim_ind[0][indmin:indmax]].values
+            except:
+                data[:,ydim_ind[0][indmin:indmax]-1] = data[:,ydim_ind[0][indmin:indmax]]
+                data[:,ydim_ind[0][indmin:indmax]+1] = data[:,ydim_ind[0][indmin:indmax]]
+                data[:,ydim_ind[0][indmin:indmax]-2] = data[:,ydim_ind[0][indmin:indmax]]
+                data[:,ydim_ind[0][indmin:indmax]+2] = data[:,ydim_ind[0][indmin:indmax]]
 
-    xdim_ind = np.where(np.nanmean(data,1)>-9e30)
-    indmin=0
-    indmax=np.size(xdim_ind)
-    if xdim_ind[0][0]<3:
-        indmin=1
-    if xdim_ind[0][-1]>(data.shape[0]-3):
-        indmax= np.size(xdim_ind)-1
-    data[xdim_ind[0][indmin:indmax]-1,:] = data[xdim_ind[0][indmin:indmax],:]
-    data[xdim_ind[0][indmin:indmax]+1,:] = data[xdim_ind[0][indmin:indmax],:]
-    data[xdim_ind[0][indmin:indmax]-2,:] = data[xdim_ind[0][indmin:indmax],:]
-    data[xdim_ind[0][indmin:indmax]+2,:] = data[xdim_ind[0][indmin:indmax],:]
-
+        xdim_ind = np.where(np.nanmean(data,1)>-9e30)
+        indmin=0
+        indmax=np.size(xdim_ind)
+        if np.size(xdim_ind[0])>0:
+            if xdim_ind[0][0]<3:
+                indmin=1
+            if xdim_ind[0][-1]>(data.shape[0]-3):
+                indmax= np.size(xdim_ind)-1
+            try:
+                data[xdim_ind[0][indmin:indmax]-1,:] = data[xdim_ind[0][indmin:indmax],:].values
+                data[xdim_ind[0][indmin:indmax]+1,:] = data[xdim_ind[0][indmin:indmax],:].values
+                data[xdim_ind[0][indmin:indmax]-2,:] = data[xdim_ind[0][indmin:indmax],:].values
+                data[xdim_ind[0][indmin:indmax]+2,:] = data[xdim_ind[0][indmin:indmax],:].values
+            except:
+                data[xdim_ind[0][indmin:indmax]-1,:] = data[xdim_ind[0][indmin:indmax],:]
+                data[xdim_ind[0][indmin:indmax]+1,:] = data[xdim_ind[0][indmin:indmax],:]
+                data[xdim_ind[0][indmin:indmax]-2,:] = data[xdim_ind[0][indmin:indmax],:]
+                data[xdim_ind[0][indmin:indmax]+2,:] = data[xdim_ind[0][indmin:indmax],:]
+    except:
+        pass
     return data
 
 def setup_grid_grid_for_plot(io):
