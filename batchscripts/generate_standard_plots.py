b1c2d78dcc5bcec47055535454bd736febf90107
Michel Bechtold <michel.bechtold@kuleuven.be>
2019-06-11 13:39:31 +0200

    DA diagnostics bat

--- batchscripts/generate_standard_plots.py
+++ batchscripts/generate_standard_plots.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python
 
+root='/scratch/leuven/317/vsc31786/output'
+root='/staging/leuven/stg_00024/OUTPUT/michelb'
+exp = 'SMAP_EASEv2_M09_SI_SMOSfw_DA'
+domain = 'SMAP_EASEv2_M09'
+outpath = '/staging/leuven/stg_00024/OUTPUT/michelb/FIG_tmp'
+
 import os
 import platform
 if platform.system() == 'Linux':
     import matplotlib
     matplotlib.use('TkAgg')
+from bat_pyldas.plotting import *
 
-
-root='/scratch/leuven/317/vsc31786/output'
-exp = 'SMAP_EASEv2_M09_SI_CLSM_SMOSfw_DA'
-domain = 'SMAP_EASEv2_M09'
-
-
-outpath = '/staging/leuven/stg_00024/OUTPUT/michelb/FIG_tmp'
 os.makedirs(outpath,exist_ok=True)
 
-from bat_pyldas.plotting import plot_catparams
+# time series
+plot_timeseries_wtd_sfmc(exp, domain, root, outpath, lat=60.0, lon=75.0)
+
+# maps
 plot_catparams(exp, domain, root, outpath)
-
-from bat_pyldas.plotting import plot_RTMparams
-#plot_RTMparams(exp, domain, root, outpath)
-
-from pyldas.visualize.plots import plot_rtm_parameters
-#plot_rtm_parameters(exp, domain, root, outpath)
-
-from bat_pyldas.plotting import plot_waterstorage_std
+plot_zbar_std(exp, domain, root, outpath)
 plot_waterstorage_std(exp, domain, root, outpath)
-
-from bat_pyldas.plotting import plot_sfmc_std
 plot_sfmc_std(exp, domain, root, outpath)
 
+
+#plot_RTMparams(exp, domain, root, outpath)
+#plot_rtm_parameters(exp, domain, root, outpath)
