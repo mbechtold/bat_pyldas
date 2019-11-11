3acd035ee0fcb76db0443f6ef2c7c927dfee0b85
Michel Bechtold <michel.bechtold@kuleuven.be>
2019-11-11 23:52:29 +0100

    vsc number read from HOME

--- batchscripts/process_ldas_output_default.py
+++ batchscripts/process_ldas_output_default.py
@@ -1,26 +1,78 @@
 #!/usr/bin/env python
 
+# batch script to convert ldas binary files to netcdf files
+# and other processed output files
+
 from pyldas.interface import LDAS_io
 import subprocess
 import os
-from bat_pyldas.functions import calc_tau_and_lag1_autocor
+import sys
+from bat_pyldas.functions import *
 from netCDF4 import Dataset
 import numpy as np
 from bat_pyldas.functions import setup_grid_grid_for_plot
 from shutil import copyfile
 from shutil import move
 
+# get vscname vscgroup
+vscgroup = os.getenv("HOME").split("/")[3]
+vscname = os.getenv("HOME").split("/")[4]
+
 # set names
-root='/scratch/leuven/329/vsc32924/output/'
-exp='INDONESIA_M09_v01_spinup2'
-domain='SMAP_EASEv2_M09'
+if len(sys.argv)<=1:
+    root='/scratch/leuven/'+vscgroup+'/'+vscname+'/output/00TEST/'
+    exp='CONGO_M09_PEATCLSMTN_v01'
+    domain='SMAP_EASEv2_M09'
+else:
+    root=sys.argv[1]
+    exp=sys.argv[2]
+    domain=sys.argv[3]
+
+date_from=None
+date_to=None
+latmin=-90.
+latmax=90.
+lonmin=-180.
+lonmax=180.
 
 # processing
+proc_daily = 1
 proc_ObsFcstAna = 0
 proc_incr = 0
-proc_daily = 1
 proc_tau_and_lag1_autocor = 0
 
+if proc_daily==1:
+    io = LDAS_io('daily', exp, domain, root)
+    io.bin2netcdf(overwrite=True,date_from=date_from,date_to=date_to,latmin=latmin,latmax=latmax,lonmin=lonmin,lonmax=lonmax)
+    #io.bin2netcdf()
+    ### add zbar
+    os.makedirs(io.paths.root +'/' + exp + '/output_postprocessed/',exist_ok=True)
+    fn = io.paths.root +'/' + exp + '/output_postprocessed/daily_zbar_images.nc'
+    os.remove(fn) if os.path.exists(fn) else None
+    copyfile(io.paths.cat+'/ens_avg'+'/daily_images.nc', fn)
+    dataset = Dataset(io.paths.root +'/' + exp + '/output_postprocessed/daily_zbar_images.nc','r+')
+    catparam = io.read_params('catparam',latmin=latmin,latmax=latmax,lonmin=lonmin,lonmax=lonmax)
+    catdef = dataset.variables['catdef']
+    [lons,lats,llcrnrlat,urcrnrlat,llcrnrlon,urcrnrlon] = setup_grid_grid_for_plot(io)
+    bf1 = np.full(lons.shape, np.nan)
+    bf1[io.grid.tilecoord.j_indg.values, io.grid.tilecoord.i_indg.values] = catparam['bf1'].values
+    bf2 = np.full(lons.shape, np.nan)
+    bf2[io.grid.tilecoord.j_indg.values, io.grid.tilecoord.i_indg.values] = catparam['bf2'].values
+    ## latmin lonmin
+    #domainlons = io.grid.ease_lons[np.min(io.grid.tilecoord.i_indg):(np.max(io.grid.tilecoord.i_indg)+1)]
+    #domainlats = io.grid.ease_lats[np.min(io.grid.tilecoord.j_indg):(np.max(io.grid.tilecoord.j_indg)+1)]
+    #bf1 = bf1[np.argmin(np.abs(domainlats-latmax)):(np.argmin(np.abs(domainlats-latmin))+1),np.argmin(np.abs(domainlons-lonmin)):(np.argmin(np.abs(domainlons-lonmax))+1)]
+    #bf2 = bf2[np.argmin(np.abs(domainlats-latmax)):(np.argmin(np.abs(domainlats-latmin))+1),np.argmin(np.abs(domainlons-lonmin)):(np.argmin(np.abs(domainlons-lonmax))+1)]
+
+    zbar = -1.0*(np.sqrt(0.000001+catdef/bf1)-bf2)
+    dataset.createVariable('zbar', 'f4', ('time', 'lat', 'lon'),chunksizes=(1, catdef.shape[1], catdef.shape[2]), fill_value=-9999.0)
+    dataset.variables['zbar'][:] = zbar
+    dataset.close()
+    move(fn, io.paths.cat+'/ens_avg'+'/daily_images.nc')
+    os.chdir(io.paths.cat+'/ens_avg')
+    ntime = io.images.time.values.__len__()
+    ncks_command = "ncks -O -4 -L 4 --cnk_dmn time,%i --cnk_dmn lat,1 --cnk_dmn lon,1 daily_images.nc daily_timeseries.nc" %  (ntime)
+    subprocess.call(ncks_command, shell=True)
 
 if proc_ObsFcstAna==1:
     io = LDAS_io('ObsFcstAna', exp, domain, root)
@@ -38,33 +90,6 @@ if proc_incr==1:
     os.chdir(io.paths.ana+'/ens_avg')
     ntime = io.images.time.values.__len__()
     ncks_command = "ncks -O -4 -L 4 --cnk_dmn time,%i --cnk_dmn lat,1 --cnk_dmn lon,1 incr_images.nc incr_timeseries.nc" %  (ntime)
-    subprocess.call(ncks_command, shell=True)
-
-if proc_daily==1:
-    io = LDAS_io('daily', exp, domain, root)
-    io.bin2netcdf(overwrite=True)
-    #io.bin2netcdf()
-    ### add zbar
-    os.makedirs(io.paths.root +'/' + exp + '/output_postprocessed/',exist_ok=True)
-    fn = io.paths.root +'/' + exp + '/output_postprocessed/daily_zbar_images.nc'
-    os.remove(fn) if os.path.exists(fn) else None
-    copyfile(io.paths.cat+'/ens_avg'+'/daily_images.nc', fn)
-    dataset = Dataset(io.paths.root +'/' + exp + '/output_postprocessed/daily_zbar_images.nc','r+')
-    catparam = io.read_params('catparam')
-    catdef = dataset.variables['catdef']
-    [lons,lats,llcrnrlat,urcrnrlat,llcrnrlon,urcrnrlon] = setup_grid_grid_for_plot(io)
-    bf1 = np.full(lons.shape, np.nan)
-    bf1[io.grid.tilecoord.j_indg.values, io.grid.tilecoord.i_indg.values] = catparam['bf1'].values
-    bf2 = np.full(lons.shape, np.nan)
-    bf2[io.grid.tilecoord.j_indg.values, io.grid.tilecoord.i_indg.values] = catparam['bf2'].values
-    zbar = -1.0*(np.sqrt(0.000001+catdef/bf1)-bf2)
-    dataset.createVariable('zbar', 'f4', ('time', 'lat', 'lon'),chunksizes=(1, catdef.shape[1], catdef.shape[2]), fill_value=-9999.0)
-    dataset.variables['zbar'][:] = zbar
-    dataset.close()
-    move(fn, io.paths.cat+'/ens_avg'+'/daily_images.nc')
-    os.chdir(io.paths.cat+'/ens_avg')
-    ntime = io.images.time.values.__len__()
-    ncks_command = "ncks -O -4 -L 4 --cnk_dmn time,%i --cnk_dmn lat,1 --cnk_dmn lon,1 daily_images.nc daily_timeseries.nc" %  (ntime)
     subprocess.call(ncks_command, shell=True)
 
 if proc_tau_and_lag1_autocor==1:
