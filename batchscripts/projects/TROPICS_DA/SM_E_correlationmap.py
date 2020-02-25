6c8821121b30d2ef7d8aa45163483895e163f6ea
vsc32460 <sebastian.apers@kuleuven.be>
2020-02-25 11:12:53 +0100

    added the SM_corr_map to master branch

--- batchscripts/projects/TROPICS_DA/SM_E_correlationmap.py
+++ batchscripts/projects/TROPICS_DA/SM_E_correlationmap.py
@@ -19,6 +19,7 @@ from pyldas.interface import LDAS_io
 def ncfile_init(fname, lats, lons, species, tags):
 
     #create the ncfile which is writable in netcdf4 format
+
     ds = Dataset(fname, 'w', 'NETCDF4')
 
     dims = ['lat','lon','species']
@@ -54,7 +55,7 @@ def filter_diagnostics_evaluation():
     ObsFcstAna = LDAS_io('ObsFcstAna', exp=exp, domain=domain, root=root)
 
     #generated nc file stored in working directory under this name
-    result_file = r'SM_E_correlationmap_drained.nc'
+    result_file = '/scratch/leuven/324/vsc32460/output/TROPICS/SM_E_correlationmap_drained.nc'
 
     tags = ['pearsonR']
 
@@ -92,7 +93,8 @@ def filter_diagnostics_evaluation():
                         ts_emissivity = df['obsobs']/df['tp1']
 
                         df = pd.concat((ts_sfmc,ts_emissivity),axis=1)
-                        ds['pearsonR'][row,col,i_spc] = df.corr().values[0,1]
+                        if not np.isnan(df.corr().values[0,1]):
+                            ds.variables['pearsonR'][row,col,i_spc] = df.corr().values[0,1]
 
                         #pearsonr([0]['obs_obs'][i_spc] - [0]['obs_fcst'][i_spc]).mean(dim='time').values
                         #tmp = [0]['obs_obs'][i_spc].values
@@ -112,6 +114,7 @@ def filter_diagnostics_evaluation():
         figpath='/data/leuven/324/vsc32460/FIG/in_situ_comparison/IN/Drained/DA_sensitivity'
         figure_single_default(data=data,lons=lons,lats=lats,cmin=cmin,cmax=cmax,llcrnrlat=llcrnrlat, urcrnrlat=urcrnrlat,
                                   llcrnrlon=llcrnrlon,urcrnrlon=urcrnrlon,outpath=figpath,exp=exp,fname=fname +'_' +figpath[52:59],plot_title='R (-), ' + fname + ' '+ figpath[52:59],cmap='jet')
+
     ds.close()
 
 if __name__ == '__main__':
